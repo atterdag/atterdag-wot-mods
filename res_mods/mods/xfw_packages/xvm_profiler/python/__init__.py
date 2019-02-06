2f383ade96c230d2568ecc42a861382a7b827983
Valdemar Lemche <valdemar@lemche.net>
2019-02-06 20:24:01 +0100

    spoter mods updated

--- res_mods/mods/xfw_packages/xvm_profiler/python/__init__.py
+++
@@ -1,6 +0,0 @@
-""" XVM (c) https://modxvm.com 2013-2018 """
-
-from xfw import IS_DEVELOPMENT
-
-if IS_DEVELOPMENT:
-    import profiler
--- res_mods/mods/xfw_packages/xvm_squad/python/__version__.py
+++
@@ -1,6 +0,0 @@
-# This file was created automatically from build script
-__xvm_version__ = '7.7.9'
-__wot_version__ = '1.3.0.1'
-__revision__ = '8732'
-__branch__ = 'default'
-__node__ = '48ded9992610ba597ee90b566b856562b49b6467'
