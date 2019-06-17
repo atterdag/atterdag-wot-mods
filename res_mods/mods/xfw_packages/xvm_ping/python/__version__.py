b70827f1ed1bad5f41cbe5011ad12b0bf6afe07c
Valdemar Lemche <valdemar@lemche.net>
2019-06-17 15:00:03 +0200

    Release 1.5.1.1

--- res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.4'
-__wot_version__ = '1.5.0.4'
-__revision__ = '9133'
-__branch__ = 'wot-1.5.0'
-__node__ = 'a0ac8b206e942eb94d6b05b4b286b797a8ab541e'
+__xvm_version__ = '7.9.5'
+__wot_version__ = '1.5.1.1'
+__revision__ = '9179'
+__branch__ = 'default'
+__node__ = '4e10e02bf1fba50d807999bd32f98a4f536bebd3'
