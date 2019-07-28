6818660e192f96706e285303d9891cbc87df4ed1
Valdemar Lemche <valdemar@lemche.net>
2019-07-28 10:55:42 +0200

    Release 1.5.1.3

--- res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_hangar/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.8'
-__wot_version__ = '1.5.1.2'
-__revision__ = '9244'
+__xvm_version__ = '7.9.9'
+__wot_version__ = '1.5.1.3'
+__revision__ = '9263'
 __branch__ = 'default'
-__node__ = 'aed96c8b2a322de278db5a9cc8d050962c758163'
+__node__ = '829eeaf0f3eb5ea786f2b6744ab8e33d17c14de0'
