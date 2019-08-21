07766760ad39ff48529d7f89442c4d66d0cd6139
Valdemar Lemche <valdemar@lemche.net>
2019-08-21 08:05:00 +0200

    Release 1.6.0.1

--- res_mods/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_tankcarousel/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.0'
-__wot_version__ = '1.6.0.0'
-__revision__ = '9287'
+__xvm_version__ = '8.0.1'
+__wot_version__ = '1.6.0.1'
+__revision__ = '9291'
 __branch__ = 'default'
-__node__ = 'fab74c10ceb15124a5290b67c3b6fd852426160e'
+__node__ = 'a3f1027d74125d2fd3b2653574cf4ad847a5888b'
