fe55752ad23e2cab6dcd5abe64500946c61d5297
Valdemar Lemche <valdemar@lemche.net>
2019-09-16 20:05:46 +0200

    Release 1.6.0.5

--- res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.3'
-__wot_version__ = '1.6.0.4'
-__revision__ = '9317'
-__branch__ = 'default'
-__node__ = 'ad2b3647e0c325fe80d19c4733e6f52e039feca2'
+__xvm_version__ = '8.0.4-dev'
+__wot_version__ = '1.6.0.5'
+__revision__ = '8990'
+__branch__ = 'wot-1.6.0'
+__node__ = '61452fbfde51b2df5590065c36199247878d5605'
