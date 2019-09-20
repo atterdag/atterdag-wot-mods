dd7a7958ee5fd82098e83718396415b1995df21c
Valdemar Lemche <valdemar@lemche.net>
2019-09-20 23:36:18 +0200

    Release 1.6.0.7-rc1

--- res_mods/mods/xfw_packages/xvm_export/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_export/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '8.0.5'
-__wot_version__ = '1.6.0.6'
-__revision__ = '9344'
+__xvm_version__ = '8.0.5-dev'
+__wot_version__ = '1.6.0.7'
+__revision__ = '9355'
 __branch__ = 'wot-1.6.0'
-__node__ = '5e4ed21cd8cde372fd4eac6f0d9ec9b042b9616a'
+__node__ = '4c56dc2dad6e339ca2aad6bf47402e9ab0ad3dc4'
